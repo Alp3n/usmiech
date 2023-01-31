7bd727c343dd2fafa4a74625f4c062f291107b42
Alp3n <adam.walukiewicz+github@relyit.pl>
2023-01-31 14:43:16 +0100

    commit before merge

---
+++ .slicemachine/assets/slices/Text/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Text';
+
+export default {
+  title: 'slices/Text'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"text":[{"type":"paragraph","text":"Anim eiusmod sint excepteur deserunt officia reprehenderit tempor Lorem exercitation laborum cupidatat. Cupidatat dolore incididunt culpa officia cupidatat eu et. Magna laboris consectetur laboris proident sunt laborum enim enim.","spans":[]}]},"id":"_Default","slice_type":"text"}} />
+_Default.storyName = ''
